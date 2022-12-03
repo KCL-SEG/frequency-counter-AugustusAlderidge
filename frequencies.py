cf4542d718a723b35d738446fefea66a6ae39860
Baiyu <baiyu.liu@kcl.ac.uk>
2022-12-03 19:27:02 +0000

    fin

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,14 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if str(item) not in frequencies:
+            frequencies[str(item)] = 1
+        else:
+            frequencies[str(item)] += 1
+
+
+
     return frequencies
+
+#print(frequencies([100, 'Hello', '100', '100', 100]))
